ed4740a0ea5adb43076e6b251516a3955a8bf3e5
tedph-cyber <tedph6@gmail.com>
2023-09-19 11:38:12 +0100

    modified:   app/About/page.tsx 	modified:   app/Offering/page.tsx 	modified:   app/Units/page.tsx 	modified:   app/Who we are/page.tsx 	modified:   next.config.js

---
+++ app/About/page.tsx
@@ -0,0 +1,9 @@
+export default function Root() {
+  return (
+    <main className="flex min-h-screen bg-white flex-col">
+      <div>
+        <h1>About</h1>
+      </div>
+    </main>
+  );
+}
--- next.config.js
+++ next.config.js
@@ -1,4 +1,28 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+  compiler: {
+    // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
+    styledComponents: boolean | {
+      // Enabled by default in development, disabled in production to reduce file size,
+      // setting this will override the default for all environments.
+      displayName?: boolean,
+      // Enabled by default.
+      ssr?: boolean,
+      // Enabled by default.
+      fileName?: boolean,
+      // Empty by default.
+      topLevelImportPaths?: string[],
+      // Defaults to ["index"].
+      meaninglessFileNames?: string[],
+      // Enabled by default.
+      cssProp?: boolean,
+      // Empty by default.
+      namespace?: string,
+      // Not supported yet.
+      minify?: boolean,
+      // Not supported yet.
+      transpileTemplateLiterals?: boolean,
+      // Not supported yet.
+      pure?: boolean,
+    },
+  },
+}
